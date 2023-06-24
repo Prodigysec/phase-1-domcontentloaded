232371b1dd68d210cbcf596c0d2afd6f0069d33a
Ian Raburu <williamraburu@gmail.com>
2023-06-23 23:47:16 -0700

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+  const modifyText = document.querySelector("#text");
+  modifyText.textContent = "This is really cool!";
+});
+
